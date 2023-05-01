d0c574875d295f6e11804f890309a059d4ab6281
yj0524 <jeonyunjae_kr@naver.com>
2023-05-01 22:16:07 +0900

    1.0.0

---
+++ README.md
@@ -0,0 +1,15 @@
+# ServerInfo (Paper 1.19.4)
+
+### 사용법
+
+`/memory` : 서버의 총 메모리와 사용 중인 메모리를 확인합니다.
+
+`/ping [target]` : target (없으면 자신) 의 핑을 확인합니다.
+
+`/userinfo [target]` : target (없으면 자신) 의 정보를 확인합니다. (Nickname, UUID, Ping)
+
+---
+
+### Libraries
+
+- [Kommand](https://github.com/monun/kommand)
---
+++ src/main/java/com/yj0524/Main.kt
@@ -0,0 +1,70 @@
+package com.yj0524
+
+import io.github.monun.kommand.*
+import org.bukkit.entity.Player
+import org.bukkit.plugin.java.JavaPlugin
+
+class Main : JavaPlugin() {
+
+    override fun onEnable() {
+        getLogger().info("Plugin Enabled")
+
+        loadKommand()
+
+        server.scheduler.runTaskTimer(this, Runnable {
+            server.onlinePlayers.forEach { player ->
+                player.setPlayerListHeaderFooter("", "Ping : ${player.ping}ms\nServer Memory : " + (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / 1024 / 1024 + "MB / " + Runtime.getRuntime().maxMemory() / 1024 / 1024 + "MB")
+            }
+        }, 0, 20)
+    }
+
+    override fun onDisable() {
+        getLogger().info("Plugin Disabled")
+    }
+
+    fun loadKommand() {
+        kommand {
+            register("memory") {
+                requires {
+                    isOp
+                }
+                executes {
+                    sender.sendMessage("§aServer Total Memory : " + Runtime.getRuntime().maxMemory() / 1024 / 1024 + "MB")
+                    sender.sendMessage("§aServer Used Memory : " + (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / 1024 / 1024 + "MB")
+                }
+            }
+            register("ping") {
+                requires {
+                    isOp
+                }
+                executes {
+                    sender.sendMessage("§aYour Ping : " + player.ping + "ms")
+                }
+                then("target" to player()) {
+                    executes {
+                        val target: Player by it
+                        sender.sendMessage("§a${target} Ping : " + target.ping + "ms")
+                    }
+                }
+            }
+            register("userinfo") {
+                requires {
+                    isOp
+                }
+                executes {
+                    sender.sendMessage("Nickname : ${player.name}")
+                    sender.sendMessage("UUID : ${player.uniqueId}")
+                    sender.sendMessage("Ping : ${player.ping}ms")
+                }
+                then("target" to player()) {
+                    executes {
+                        val target: Player by it
+                        sender.sendMessage("Nickname : ${target.name}")
+                        sender.sendMessage("UUID : ${target.uniqueId}")
+                        sender.sendMessage("Ping : ${target.ping}ms")
+                    }
+                }
+            }
+        }
+    }
+}
